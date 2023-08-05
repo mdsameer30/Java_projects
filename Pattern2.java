419f0016be59cbe1bcdb05cd295bf47b06f75ecf
Mohammad Sameer <138815615+mdsameer30@users.noreply.github.com>
2023-08-05 19:23:40 +0530

    Update Pattern2.java
    Hollow rectangle pattern

--- Pattern2.java
+++ Pattern2.java
@@ -1,6 +1,6 @@
 public class Pattern2 {
     public static void main(String args[]){
-        //hollow rectangle
+        //hollow rectangle pattern
         for(int i=0; i<=4; i++){
             for(int j=0; j<=5; j++){
                 if(i==0 || j==0 || i==4 ||j==5){
