ed4ba54f2078530b0e3dd0806802f8fc2f65e2a0
Mohammad Sameer <138815615+mdsameer30@users.noreply.github.com>
2023-08-06 01:03:21 +0530

    Hollow rectangle pattern

---
+++ Pattern2.java
@@ -0,0 +1,16 @@
+public class Pattern2 {
+    public static void main(String args[]){
+        //hollow rectangle
+        for(int i=0; i<=4; i++){
+            for(int j=0; j<=5; j++){
+                if(i==0 || j==0 || i==4 ||j==5){
+                System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+    
+}
